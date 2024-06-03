26d5041d04e7b1c7e21f10c2228d427abaee532b
DESKTOP-CN1G0RI\Damian <damian.luis,porta@gmail.com>
2024-06-03 19:48:32 -0300

    constants added

--- Dockerfile
+++ Dockerfile
@@ -10,16 +10,17 @@ COPY package*.json ./
 # Instala las dependencias del proyecto
 RUN npm install || true
 
+# Copia el resto de los archivos de la aplicación
+COPY . .
+
 # Instala globalmente las herramientas necesarias
 RUN npm install -g next
 RUN npm install -g ts-node
-
-# Copia el resto de los archivos de la aplicación
-COPY . .
 
 # Expone el puerto en el que la aplicación estará corriendo
 EXPOSE 3006
 
 # Comando para correr la aplicación
 CMD ["npm", "start"]
+
 
---
+++ constant/constants.tsx
@@ -0,0 +1,17 @@
+export const HTTP_CODES = {
+    SUCCESS: 200,
+    CREATED: 201,
+    NOT_FOUND: 404,
+    BAD_REQUEST: 400,
+    INTERNAL_SERVER_ERROR: 500
+  };
+  
+  export const RESPONSE_MESSAGES = {
+    BOOK_UPLOADED_SUCCESSFULLY: 'Book uploaded successfully',
+    BOOK_DELETED_SUCCESSFULLY: 'Book deleted successfully',
+    QUESTION_ANSWERED_SUCCESSFULLY: 'Question answered successfully',
+    BOOK_NOT_FOUND: 'Book not found',
+    INVALID_REQUEST: 'Invalid request',
+    INTERNAL_SERVER_ERROR: 'Internal server error'
+  };
+  
